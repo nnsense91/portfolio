e6e8bba4dddf6b4543d552d1a08c57e15f151c8d
Ivan Egorov <HD-74@yandex.ru>
2019-07-10 00:51:33 +0500

    Skills in Vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,49 @@
-console.log("this is skills module");
+import Vue from "vue";
+
+const skill = {
+    template: "#skill",
+    props: {
+        skillName: String,
+        skillPercent: Number
+    },
+    methods: {
+        drawCircle() {
+            const circle = this.$refs["circle"];
+            const dashArray = parseInt(
+                getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+            );
+            const percent = (dashArray / 100) * (100 - this.skillPercent);
+            circle.style.strokeDashoffset = percent;
+        }
+    },
+    mounted() {
+        this.drawCircle();
+    }
+}
+
+const row = {
+    template: "#skills-row",
+    props: {
+        skill: Object
+    },
+    components: {
+        skill
+    }
+}
+
+new Vue({
+    el: "#skills-container",
+    template: "#skills-list",
+    components: {
+        row
+    },
+    data() {
+        return {
+            skills: []
+        }
+    },
+    created() {
+        const data = require("../data/skills-data.json");
+        this.skills = data;
+    }
+})
