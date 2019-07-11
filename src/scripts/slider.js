2e8cc18ada0f7aeff571b4c1c3097f8e711a31e5
unknown <C:\Users\HD_Work\OneDrive\Documents\\>
2019-07-11 15:30:31 +0500

    projects slider completed

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,31 +1,53 @@
 import Vue from "vue";
-import { create } from "domain";
 
 const btns = {
-    template: "#slider-btns"
-}
+    template: "#slider-btns",
+    props: {
+        currentIndex: Number
+    }
+};
 
 const thumbs = {
-    template: "#slider-thumbs"
-}
+    template: "#slider-thumbs",
+    props: {
+        works: Array,
+        currentWork: Object
+    }
+};
 
 const display = {
     template: "#slider-display",
+    props: {
+        works: Array,
+        currentWork: Object,
+        currentIndex: Number
+    },
     components : {
         btns, thumbs
     }
-}
+};
 
 const tags = {
-    template: "#slider-tags"
-}
+    template: "#slider-tags",
+    props: {
+        tags: Array
+    }
+};
 
 const info = {
     template: "#slider-info",
+    props: {
+        currentWork: Object
+    },
+    computed: {
+        tagsArray() {
+            return this.currentWork.skills.split(", ");
+        }
+    },
     components: {
         tags
     }
-}
+};
 
 new Vue({
     el: "#slider-component",
@@ -35,11 +57,46 @@ new Vue({
     },
     data() {
         return {
-            woks:[]
+            works:[],
+            currentIndex: 0
+        }
+    },
+    computed: {
+        currentWork() {
+            return this.works[this.currentIndex];
+        }
+    },
+    watch: {
+        currentIndex(value) {
+            const worksLengthFromZero = this.works.length - 1;
+            if (value < 0) this.currentIndex = worksLengthFromZero;
+            if (value > worksLengthFromZero) this.currentIndex = 0;
+        }
+    },
+    methods: {
+        makeArrWithRequiredImages(data) {
+            return data.map(item => {
+                const requirePic = require(`../images/content/${item.photo}`);
+
+                item.photo = requirePic;
+
+                return item
+            })
+        },
+        handleSlide(direction) {
+            switch(direction) {
+                case "next":
+                    this.currentIndex++;
+                    break;
+                case "prev":
+                    this.currentIndex--;
+                    break;
+            }
         }
     },
     created() {
         const data = require("../data/slider-data.json");
-        this.works = data;
+
+        this.works = this.makeArrWithRequiredImages(data);
     }
-})
+});
