d17341bbdd79ca63935fc815d978c690caef1ca7
Ivan Egorov <HD-74@yandex.ru>
2019-07-15 07:46:23 +0500

    admin panel started

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,7 @@
 <template lang="pug">
   h1 Welcome to the Vue App
 </template>
+
+
+<style lang="postcss" scoped>
+</style>
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -72,6 +72,8 @@ new Vue({
     methods: {
         findCircle() {
             let circleBlock = this.$refs["skills-block"];
+            console.log(circleBlock);
+
             return {
                 findTop: circleBlock
             };
