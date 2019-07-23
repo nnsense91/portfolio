2f730dce89845710b6406d27500f1d6f6064ad35
Ivan Egorov <HD-74@yandex.ru>
2019-07-23 23:34:05 +0500

    form validation in progress..

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -5,7 +5,8 @@ class Validate {
         this.email = "j-email";
         this.text = "j-text";
         this.btn = "j-submit-btn";
-        this.blockClass = "j-block";
+        this.block = "j-block";
+        this.errorBlock = "j-error-block";
 
         this.errorClass = "has-error";
 
@@ -30,17 +31,91 @@ class Validate {
         this.$name = this.$form.querySelector(`.${this.name}`);
         this.$inputName = this.$name.querySelector("input");
         this.$email = this.$form.querySelector(`.${this.email}`);
-        this.$inputEmail = this.$name.querySelector("input");
+        this.$inputEmail = this.$email.querySelector("input");
         this.$textarea = this.$form.querySelector(`.${this.text}`);
-        this.$inputTextArea = this.$name.querySelector("textarea");
+        this.$inputTextArea = this.$textarea.querySelector("textarea");
     }
 
     _bindEvents() {
         this.$btn.addEventListener("click", (event) => {
             event.preventDefault();
-        })
+
+            const isFormFullFilled = this.checkForm();
+            const name = this.$inputName.value;
+            const mail = this.$inputEmail.value;
+            const comment = this.$inputTextArea.value;
+
+            let formData = new FormData();
+
+            formData.append("name", name);
+            formData.append("comment", comment);
+            formData.append("to", mail);
+
+            fetch(`http://webdev-api.loftschool.com/sendemail`, {
+                method: "POST",
+                body: formData
+            }).then((response) => {
+                return response.json();
+            }).then((info) => {
+                return info.message;
+            }).then((message) => {
+                // popup
+                this.$form.reset();
+            }).catch(() => {
+                // popup
+            })
+        });
+
+        this.$inputName.addEventListener("change", (event) => {
+            this.inputChangeHandler(event, "name");
+
+        });
+
+        this.$inputEmail.addEventListener("change", (event) => {
+            const isValidEmail = event.target.checkValidity();
+            const emailStr = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            const regEmail = new RegExp(emailStr, "u");
+
+            if (isValidEmail && regEmail.test(this.$inputEmail.value)) {
+                this.inputChangeHandler(event, "email");
+            } else if (isValidEmail === false || regEmail.test(this.$inputEmail.value) === false) {
+                //вывести ошибку о некорректном адресе
+                console.log(this.incorrectMessage);
+
+            }
+        });
+
+        this.$inputTextArea.addEventListener("change", (event) => {
+            this.inputChangeHandler(event, "text");
+        });
+    }
+
+    inputChangeHandler(event, inputName) {
+        if (event.target.value.length) {
+            this.isFieldCorrect[inputName] = true;
+            // убираем сообщение с ошибкой(пустое поле)
+            console.log("Удаляем сообщение об ошибке");
+        } else {
+            this.isFieldCorrect[inputName] = false;
+            this.showErrorMessage(event.target, this.emptyErrorMessage);
+        }
+    }
+
+    checkForm() {
+
+    }
+
+    showErrorMessage(element, message) {
+        const parentFormBlock = element.closest(`.${this.block}`);
+        const messageEl = parentFormBlock.querySelector(`.${this.errorBlock}`)
+
+        messageEl.innerText = "";
+        messageEl.classList.add(this.errorClass);
+        message.innerText = message;
     }
 
 }
 
 export default Validate;
+
+//1.36.0
