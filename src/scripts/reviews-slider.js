adc1e2965130acc3cb3e67b9741c23f35018bdea
unknown <C:\Users\HD_Work\OneDrive\Documents\\>
2019-07-24 13:12:48 +0500

    reviews slider buttons disabled if reviews in start or finish position

--- src/scripts/reviews-slider.js
+++ src/scripts/reviews-slider.js
@@ -16,6 +16,7 @@ new Vue({
         review
     },
     data: {
+        flag: 0,
         reviews: [],
         flickityOptions: {
             initialIndex: 3,
@@ -35,15 +36,41 @@ new Vue({
             })
         },
         next() {
-            this.$refs.flickity.next();
+            this.$refs.prev.classList.remove("reviews__button--disabled")
+            this.$refs.next.classList.remove("reviews__button--disabled")
+            if (this.flag < this.reviews.length/2 - 1) {
+                this.$refs.flickity.next();
+                this.flag++;
+                if (this.flag >= this.reviews.length/2 - 1) {
+                    this.$refs.next.classList.add("reviews__button--disabled")
+                }
+            }
+            console.log(this.flag);                        
         },
-
         previous() {
-            this.$refs.flickity.previous();
+            this.$refs.prev.classList.remove("reviews__button--disabled")
+            this.$refs.next.classList.remove("reviews__button--disabled")
+            if (this.flag > 0) {
+                this.$refs.flickity.previous();
+                this.flag--;
+                if (this.flag <= 0){
+                    this.$refs.prev.classList.add("reviews__button--disabled")
+                }
+            }
+                
+            console.log(this.flag);
         }
     },
     created() {
         const data = require("../data/reviews.json")
         this.reviews = this.makeArrWithRequiredImages(data)
+    },
+    mounted() {
+        console.log(this.flag)
+        if (this.flag <= 0) {
+            this.$refs.prev.classList.add("reviews__button--disabled")
+        } else if (this.flag >= this.reviews.length/2 - 1) {
+            this.$refs.next.classList.add("reviews__button--disabled")
+        }
     }
 })
