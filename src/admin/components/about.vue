3be1473120563b90038144e88a7e61aee80ecc16
unknown <C:\Users\HD_Work\OneDrive\Documents\\>
2019-08-05 18:15:57 +0500

    admin divided to components (still in progress)

--- src/admin/components/about.vue
+++ src/admin/components/about.vue
@@ -80,3 +80,199 @@
                       input(type="text" value="100").skills-form__new-skillpercent
                       button(type="button" title="Добавить новый навык").btn-addnew__plus.skills-form__btn-addnew--skill +
 </template>
+
+<style lang="postcss" scoped>
+
+
+.btn-main {
+  text-transform: uppercase;
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  border-radius: 25px;
+  padding: 20px 50px;
+  background: linear-gradient(to right, #006aed, #3f35cb);
+
+  @media screen and (max-width: 440px) {
+    font-size: 14px;
+    padding: 10px 25px;
+  }
+}
+
+.btn-discard {
+  width: 15px;
+  height: 12px;
+  background: svg-load("cross.svg", fill=#bf2929, width=100%, height=100%) 0
+    100% / contain no-repeat;
+}
+
+.btn-edit {
+  width: 14px;
+  height: 14px;
+  background: svg-load("pencil.svg", fill=#a0a5b1, width=100%, height=100%) 0
+    100% / contain no-repeat;
+}
+
+.about {
+  height: 100%;
+  padding-top: 60px;
+  padding-bottom: 60px;
+  background-image: url("../../images/content/mountain.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+.about__title-row {
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 420px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+
+.title {
+  font-size: 21px;
+  font-weight: bold;
+  color: #414c63;
+}
+
+.about__addgroup {
+  margin-left: 100px;
+
+  @media screen and (max-width: 420px) {
+    margin-left: 0;
+  }
+}
+
+.form__btn-addnew--group {
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+}
+
+.about__addgroup-text {
+  margin-left: 13px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #383bcf;
+}
+
+.about__content {
+  margin-top: 60px;
+}
+
+//skills
+
+.skills__group {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.skills__group-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -2%;
+}
+
+.skills-item {
+  margin-top: 20px;
+  margin-left: 2%;
+  padding: 2%;
+  width: 48%;
+  height: 387px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(122, 122, 122, 0.1);
+
+  @media screen and (max-width: 420px) {
+    width: 100%;
+  }
+}
+
+.skills-form {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.skills-form__groupname-block {
+  margin-left: 2%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+}
+
+.skills-form__groupname {
+  width: 60%;
+}
+
+.skills-form__groupname-control {
+  width: 10%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.skills-form__content {
+  height: 70%;
+  padding-top: 20px;
+  margin-left: 2%;
+  font-size: 16px;
+  color: #414c63;
+}
+
+.skills-form__skills-table {
+  width: 100%;
+}
+
+.skills-form__skills-row {
+  width: 100%;
+}
+
+.skills-form__skills-cell--name {
+  width: 60%;
+}
+
+.skills-form__skills-cell--value {
+  width: 8%;
+}
+.skills-form__skills-cell--percent {
+  width: 15%;
+}
+
+.skills-form__skills-cell--control {
+  width: 10%;
+}
+
+.skills-form__skills-control {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.skills-form__new-skillblock {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.skills-form__new-skillname {
+  width: 50%;
+}
+.skills-form__new-skillpercent {
+  margin-left: 10px;
+  width: 13%;
+}
+.skills-form__new-skillname,
+.skills-form__new-skillpercent {
+  font-size: 16px;
+  padding-bottom: 2px;
+}
+.skills-form__btn-addnew--skill {
+  font-size: 30px;
+  margin-left: 20px;
+}
+</style>
