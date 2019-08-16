a181a30cf13b74fe02c63ef48d03b13d8c115652
unknown <C:\Users\HD_Work\OneDrive\Documents\\>
2019-08-16 12:07:18 +0500

    webpack admin-fix - 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,3 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@nnsense91 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
- 
-1
-0 0 Obzhigalov/advanced
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Security  Insights
-advanced/webpack.config.js
-@Obzhigalov Obzhigalov BuildAdmin
-dc6cae0 14 days ago
-186 lines (172 sloc)  4.05 KB
-    
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -31,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = './';
+  const publicPath = "";
 
   const pcss = {
     test: /\.(p|post|)css$/,
@@ -52,6 +30,7 @@ module.exports = (env, argv) => {
     loader: "babel-loader",
     exclude: /node_modules/,
     options: {
+      presets: ["@babel/preset-env"],
       plugins: ["@babel/plugin-syntax-dynamic-import"]
     }
   };
@@ -105,6 +84,16 @@ module.exports = (env, argv) => {
   };
 
   const config = {
+    entry: {
+      main: ["@babel/polyfill", "./src/main.js"],
+      admin: ["@babel/polyfill", "./src/admin/main.js"]
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "[name].[hash].build.js",
+      publicPath: isProductionBuild ? publicPath : "",
+      chunkFilename: "[chunkhash].js"
+    },
     module: {
       rules: [pcss, vue, js, files, svg, pug]
     },
@@ -164,7 +153,7 @@ module.exports = (env, argv) => {
       new OptimizeCSSAssetsPlugin({})
     ];
   }
-  
+
   const mainConfig = {
     ...config,
     entry: {
@@ -206,15 +195,3 @@ module.exports = (env, argv) => {
   
    return [mainConfig, adminConfig];
 };
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
