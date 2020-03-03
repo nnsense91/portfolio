e79fe12198ddb921d0306bc71102548b60c1f43c
nnsense91 <HD-74@yandex.ru>
2020-03-03 16:29:15 +0500

    for gh-pages

--- src/admin/requests.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const token = localStorage.getItem("token");
-
-axios.defaults.baseURL = "https://webdev-api.loftschool.com/";
-axios.defaults.headers["Authorization"] = `Bearer ${token}`;
-
-export default axios;
