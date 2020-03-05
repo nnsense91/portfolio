d7d17b66d8907ab9cf391cedaf0a1f7fe11bb3e4
nnsense91 <HD-74@yandex.ru>
2020-03-05 17:10:15 +0500

    if user unlogged

--- src/admin/router.js
+++ src/admin/router.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import axios from 'axios';
+import store from './store';
 
 Vue.use(VueRouter);
+
+const baseURL = "https://webdev-api.loftschool.com/"
+const guard = axios.create({
+	baseURL
+});
 
 const routes = [
     {
         path: "/",
-        component: () => import ("./components/about.vue")
+				component: () => import ("./components/about.vue")
     },
     {
         path: "/works",
@@ -18,8 +25,35 @@ const routes = [
     },
     {
         path: "/login",
-        component: () => import ("./components/login.vue")
+				component: () => import ("./components/login.vue"),
+				meta: {
+					public: true
+				}
     }
 ];
 
-export default new VueRouter({ routes });
+const router = new VueRouter({ routes });
+
+router.beforeEach(async (to, from, next) => {
+	const isPublicRoute = to.matched.some(record => record.meta.public);
+	const isUserLogged = store.getters["user/userIsLogged"];
+
+	if (isPublicRoute === false && isUserLogged === false) {
+		const token = localStorage.getItem("token");
+		guard.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+		try {
+			const response = await guard.get("/user");
+			store.commit("user/SET_USER", response.data.user);
+			next();
+		} catch (error) {
+			router.push('/login');
+			localStorage.removeItem("token");
+		}
+	} else {
+		next();
+	}
+	
+});
+
+export default router;
