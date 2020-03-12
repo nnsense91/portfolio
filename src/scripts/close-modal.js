5d083ddf21bbb40a5358dc9cadcd08896b620ff7
Ivan Egorov <HD-74@yandex.ru>
2020-03-12 19:31:10 +0500

    build

--- src/scripts/close-modal.js
+++
@@ -1,7 +0,0 @@
-const closeBtn = document.querySelector(".popup-modal__close-btn");
-const pop = document.querySelector(".j-popup-modal");
-
-closeBtn.addEventListener("click", () => {
-    pop.classList.add("popup-modal--hidden");
-    document.body.classList.remove("hidden");
-});
